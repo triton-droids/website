ee4f609c36a4b5db3bce7910a96ee07495802fe3
andrewy1n <andrewyingo@gmail.com>
2026-01-19 00:23:23 +0000

    Init React + Vite + Typescript + Tailwind, deployment script

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Join from './pages/Join'
+import Team from './pages/Team'
+import Projects from './pages/Projects'
+import Sponsorship from './pages/Sponsorship'
+import Advisor from './pages/Advisor'
+import Alumni from './pages/Alumni'
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sponsorship" element={<Sponsorship />} />
+        <Route path="/advisor" element={<Advisor />} />
+        <Route path="/alumni" element={<Alumni />} />
+      </Routes>
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/pages/Sponsorship.tsx
@@ -0,0 +1,29 @@
+export default function Sponsorship() {
+  return (
+    <>
+      <h1 className="text-8xl font-bold">Fund a droid. Make a difference!</h1>
+
+      <article className="sponsor-info">
+        <h2 className="text-5xl font-bold">No contribution is too small.</h2>
+        <p className="text-xl leading-relaxed">
+          Be a part of transforming the future of robotics at UCSD! Triton Droids
+          is embarking on a mission to create a state-of-the-art humanoid robot
+          that will redefine technology and innovation. Your support will enable
+          students to acquire practical experience and engage in pioneering
+          research. Together, we can cultivate the next wave of robotics and
+          inspire the leaders of tomorrow!
+        </p>
+      </article>
+
+      <section className="donate-info flex flex-col gap-4 mt-8">
+        <button className="btn-primary">DONATE NOW</button>
+        <a href="#" className="text-accent">
+          View our Sponsorship Package
+        </a>
+        <p className="text-muted-text">
+          Contact for pitch presentation: tritondroids@ucsd.edu
+        </p>
+      </section>
+    </>
+  )
+}
