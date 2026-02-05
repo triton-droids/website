6fd4b0a9cff6bf86ecdd1aeacf6da11cd812325e
Andrew Yin <67555036+andrewy1n@users.noreply.github.com>
2026-02-04 21:05:33 -0800

    Footer Responsiveness Fix (#86)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,70 +4,70 @@ import ucsdLogo from '../assets/UCSanDiegoLogo-White 1.png';
 
 export default function Footer() {
   return (
-    <footer className="w-full mt-auto bg-main-bg grid grid-cols-[1fr_auto_1fr] items-start gap-36 p-20">
-      <div className="inline-flex flex-col justify-start items-start gap-6 justify-self-end">
-        <img className="w-52 h-auto" src={logo} alt="Triton Droids" />
-        <div className="w-96 justify-start text-neutral-100 text-base font-normal font-sans">
+    <footer className="w-full mt-auto bg-main-bg grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[1fr_auto_1fr] items-start gap-10 md:gap-12 lg:gap-20 xl:gap-36 p-6 md:p-10 lg:p-20">
+      <div className="inline-flex flex-col justify-start items-start gap-6 md:justify-self-start lg:justify-self-end">
+        <img className="w-40 md:w-52 h-auto" src={logo} alt="Triton Droids" />
+        <div className="max-w-96 justify-start text-neutral-100 text-sm md:text-base font-normal font-sans">
           Triton Droids is a non profit and is classified as a
           &apos;Unincorporated association&apos; organization (EIN 33- 1874028)
           by the Internal Revenue Service (IRS).
         </div>
       </div>
-      <div className="inline-flex justify-start items-start gap-6">
-        <div className="w-24 inline-flex flex-col justify-start items-start gap-16">
+      <div className="inline-flex justify-start items-start gap-12 md:gap-6">
+        <div className="inline-flex flex-col justify-start items-start gap-4 md:gap-16">
           <h4 className="text-main-text font-semibold text-base mb-1">
             Explore
           </h4>
-          <div className="w-20 flex flex-col justify-start items-start gap-6">
+          <div className="flex flex-col justify-start items-start gap-3 md:gap-6">
             <Link
               to="/join"
-              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-base font-normal font-sans"
+              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-sm md:text-base font-normal font-sans"
             >
               Join
             </Link>
             <Link
               to="/team"
-              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-base font-normal font-sans"
+              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-sm md:text-base font-normal font-sans"
             >
               Our Team
             </Link>
             <Link
               to="/alumni"
-              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-base font-normal font-sans"
+              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-sm md:text-base font-normal font-sans"
             >
               Alumni
             </Link>
             <Link
               to="/advisor"
-              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-base font-normal font-sans"
+              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-sm md:text-base font-normal font-sans"
             >
               Advisors
             </Link>
             <Link
               to="/projects"
-              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-base font-normal font-sans"
+              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-sm md:text-base font-normal font-sans"
             >
               Projects
             </Link>
             <Link
               to="/sponsorship"
-              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-base font-normal font-sans"
+              className="no-underline hover:text-accent transition-colors justify-start text-neutral-100 text-sm md:text-base font-normal font-sans"
             >
               Sponsors
             </Link>
           </div>
         </div>
 
-        <div className="w-24 inline-flex flex-col justify-start items-start gap-16">
+        <div className="inline-flex flex-col justify-start items-start gap-4 md:gap-16">
           <div className="justify-start text-neutral-100 text-base font-bold font-sans">
             Socials
           </div>
-          <div className="w-16 flex flex-col justify-start items-start gap-6">
+          <div className="flex flex-col justify-start items-start gap-3 md:gap-6">
             <a
               href="https://instagram.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="justify-start text-neutral-100 text-base font-normal font-sans hover:text-accent transition-colors cursor-pointer"
+              className="justify-start text-neutral-100 text-sm md:text-base font-normal font-sans hover:text-accent transition-colors cursor-pointer"
             >
               Instagram
             </a>
@@ -75,7 +75,7 @@ export default function Footer() {
               href="https://linkedin.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="justify-start text-neutral-100 text-base font-normal font-sans hover:text-accent transition-colors cursor-pointer"
+              className="justify-start text-neutral-100 text-sm md:text-base font-normal font-sans hover:text-accent transition-colors cursor-pointer"
             >
               LinkedIn
             </a>
@@ -83,7 +83,7 @@ export default function Footer() {
               href="https://linktr.ee"
               target="_blank"
               rel="noopener noreferrer"
-              className="justify-start text-neutral-100 text-base font-normal font-sans hover:text-accent transition-colors cursor-pointer"
+              className="justify-start text-neutral-100 text-sm md:text-base font-normal font-sans hover:text-accent transition-colors cursor-pointer"
             >
               Linktree
             </a>
@@ -91,7 +91,7 @@ export default function Footer() {
               href="https://discord.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="justify-start text-neutral-100 text-base font-normal font-sans hover:text-accent transition-colors cursor-pointer"
+              className="justify-start text-neutral-100 text-sm md:text-base font-normal font-sans hover:text-accent transition-colors cursor-pointer"
             >
               Discord
             </a>
@@ -99,9 +99,13 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="inline-flex flex-col justify-start items-start gap-6">
-        <img className="w-80 h-16" src={ucsdLogo} alt="UC San Diego" />
-        <div className="self-stretch justify-start text-neutral-100 text-base font-normal font-sans">
+      <div className="inline-flex flex-col justify-start items-start gap-4 md:gap-6 md:col-span-2 lg:col-span-1">
+        <img
+          className="w-48 md:w-64 lg:w-80 h-auto object-contain"
+          src={ucsdLogo}
+          alt="UC San Diego"
+        />
+        <div className="self-stretch justify-start text-neutral-100 text-sm md:text-base font-normal font-sans">
           9500 Gilman Dr, La Jolla, CA 92093
         </div>
       </div>
